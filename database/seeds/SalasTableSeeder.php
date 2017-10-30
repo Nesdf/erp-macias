f1e7d7b54481bbbd43c3520505e6f5b723533c4e
Nestor Perez <nes64df@gmail.com>
2017-10-30 13:45:33 -0600

    Se agregan seeder

---
+++ database/seeds/SalasTableSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class SalasTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $salas = [
+            'IDF 1', 
+            'IDF 2', 
+            'IDF 3', 
+            'IDF 4', 
+            'IDF 5', 
+            'IDF 6', 
+            'IDF 7', 
+            'IDF 8',
+            'IDF 9',
+            'Sala De Cine',
+            'Monte Carlo',
+            'Saint Tropez', 
+            'Capri', 
+            'Tulum', 
+            'Fortaleza', 
+            'Recife', 
+            'Porto Alegre', 
+            'Belo Horizonte',
+            'Cancun',
+            'Los Cabos'
+        ];
+		$carbon = new \Carbon\Carbon();
+        $num = 0;
+        foreach ($salas as $key => $value) {
+            DB::table('salas')->insert([
+                'sala' => $salas[$num],
+                'created_at' => $carbon->now()->format('Y-m-d H:i:s'),
+                'updated_at' => $carbon->now()->format('Y-m-d H:i:s')
+            ]);
+            $num++;
+        }
+    }
+}
---
+++ database/seeds/ViasTableSeeder.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ViasTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $carbon = new \Carbon\Carbon();
+		DB::table('users')->insert([
+			'via' => 'Aspera',
+			'created_at' => $carbon->now()->format('Y-m-d H:i:s'),
+			'updated_at' => $carbon->now()->format('Y-m-d H:i:s')
+		]);
+		DB::table('users')->insert([
+			'Fox Fast' => 'Aspera',
+			'created_at' => $carbon->now()->format('Y-m-d H:i:s'),
+			'updated_at' => $carbon->now()->format('Y-m-d H:i:s')
+		]);
+		DB::table('users')->insert([
+			'WB DETE' => 'Aspera',
+			'created_at' => $carbon->now()->format('Y-m-d H:i:s'),
+			'updated_at' => $carbon->now()->format('Y-m-d H:i:s')
+		]);
+		DB::table('users')->insert([
+			'WB MPI ASPERA' => 'Aspera',
+			'created_at' => $carbon->now()->format('Y-m-d H:i:s'),
+			'updated_at' => $carbon->now()->format('Y-m-d H:i:s')
+		]);
+		DB::table('users')->insert([
+			'WB MPI ASPERA' => 'Aspera',
+			'created_at' => $carbon->now()->format('Y-m-d H:i:s'),
+			'updated_at' => $carbon->now()->format('Y-m-d H:i:s')
+		]);
+		DB::table('users')->insert([
+			'DELUXE MEDIA' => 'Aspera',
+			'created_at' => $carbon->now()->format('Y-m-d H:i:s'),
+			'updated_at' => $carbon->now()->format('Y-m-d H:i:s')
+		]);
+    }
+}

eb589c47ab3cde657e0ed177d57347abb668f821
Nestor Perez <nes64df@gmail.com>
2017-10-31 06:56:02 -0600

    modificación en la migración de llamados

--- database/migrations/2017_10_17_160535_create_calendar_llamados_table.php
+++ database/migrations/2017_10_17_160535_create_calendar_llamados_table.php
@@ -16,9 +16,14 @@ class CreateCalendarLlamadosTable extends Migration
         //
         Schema::create('calendario', function(Blueprint $table){
             $table->increments('id');
-            $table->string('evento', 60);
-            $table->datetime('horario');
-            $table->string('folio', 20)->nullable();;
+            $table->string('actor', 50);
+            $table->string('cita_start', 50);
+            $table->string('cita_end', 50);
+            $table->string('folio_id', 20)->nullable();
+            $table->boolean('estatus_grupo', 50);
+            $table->boolean('estatus', 50);
+            $table->text('descripcion', 50);
+            $table->string('director', 50);
             $table->timestamps();
         });
     }
@@ -31,5 +36,6 @@ class CreateCalendarLlamadosTable extends Migration
     public function down()
     {
         //
+        Schema::dropIfExists('calendario');
     }
 }

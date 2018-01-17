d72c69595ad6a7c525c92872a2dae1806eab19e7
NESTOR EMILIO PEREZ SANCHEZ <ing.nestor.sanz@gmail.com>
2018-01-17 17:18:28 -0600

    Se quita certificado de seguridad

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,8 +9,8 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
 
 		<!-- bootstrap & fontawesome √ -->
-		<link rel="stylesheet" @if(getenv('APP_ENV') == 'Development') href="{{ asset('assets/dashboard/css/bootstrap.min.css') }}"  @endif/>
-		<link rel="stylesheet" @if(getenv('APP_ENV') == 'Production') href="{{ secure_asset('assets/dashboard/css/bootstrap.min.css') }}" @endif />
+		<link rel="stylesheet" href="{{ asset('assets/dashboard/css/bootstrap.min.css') }}"/>
+		<link rel="stylesheet" href="{{ secure_asset('assets/dashboard/css/bootstrap.min.css') }}"/>
 		<link rel="stylesheet" href="{{ asset('assets/dashboard/font-awesome/4.5.0/css/font-awesome.min.css') }}" />
 
 		<!-- page specific plugin styles -->

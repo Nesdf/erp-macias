7ad41b5799960e4c2b0f140faeb8640217f7f87b
nestor <nes64df@gmail.com>
2018-02-19 08:37:01 -0600

    Se agrega fecha de rayado en fecha script

--- Modules/MgEpisodios/Resources/views/index.blade.php
+++ Modules/MgEpisodios/Resources/views/index.blade.php
@@ -1240,12 +1240,12 @@
 								$('input[name=chk_reprobacion]').prop('checked', true)
 							}
 
-							if(data.sin_script == false){
+							/*if(data.sin_script == false){
 								var fechaScript = '';
 								if(data.fecha_script != null){
 									fechaScript = data.fecha_script;
-								}
-								$('#add_date_script').html('<label>Fecha de script</label><input type="text" id="fecha_script2" name="fecha_script" class="form-control" value="'+fechaScript+'" required></input>');
+								}*/
+								$('#add_date_script').html('<label>Fecha de script</label><input type="text" id="fecha_script2" name="fecha_script" class="form-control" value="'+data.rayado+'" required></input>');
 
 								$('#fecha_doblaje_update, #fecha_script2').datepicker({
 									dateFormat: "yy-mm-dd",

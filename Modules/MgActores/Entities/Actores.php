03fd81dcbc377c8b7d7c28df62aea2d88924b317
Ing Nestor Emilio Perez Sanchez <ineps@MacBook-Air-de-Ing.local>
2018-03-17 09:59:39 -0600

    se agrega campo rfc

--- Modules/MgActores/Entities/Actores.php
+++ Modules/MgActores/Entities/Actores.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Actores extends Model
 {
 	protected $table = 'actores';
-    protected $fillable = ['nombre_completo', 'nombre_artistico', 'created_at', 'updated_at'];
+    protected $fillable = ['nombre_completo', 'nombre_artistico', 'rfc', 'created_at', 'updated_at'];
 }

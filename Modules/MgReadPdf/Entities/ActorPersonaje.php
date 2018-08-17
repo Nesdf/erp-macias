dee679f90bc4c514e0019bdfbab5ecb0bcf6e6e4
nestor <nes64df@gmail.com>
2018-08-17 01:19:35 -0500

    Se agrega campo asignado

--- Modules/MgReadPdf/Entities/ActorPersonaje.php
+++ Modules/MgReadPdf/Entities/ActorPersonaje.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class ActorPersonaje extends Model
 {
     protected $table = 'actor_personaje'; 
-    protected $fillable = ['id', 'episodio_folio', 'personaje', 'fijo', 'loops', 'proyecto', 'created_at', 'updated_at'];
+    protected $fillable = ['id', 'episodio_folio', 'personaje', 'fijo', 'loops', 'proyecto', 'asignado',  'created_at', 'updated_at'];
 
 
     

94cf20a9cda7bbee1f4f027fc32a0ad2defc2f02
NESTOR EMILIO PEREZ SANCHEZ <ing.nestor.sanz@gmail.com>
2020-04-22 10:17:16 -0500

    Actualizar

--- Modules/MgCalendar/Entities/Episodios.php
+++ Modules/MgCalendar/Entities/Episodios.php
@@ -7,13 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Episodios extends Model
 {
 	protected $table = 'episodios';
-    protected $fillable = ['titulo_original', 'num_episodio', 'proyectoId', 'salaId', 'productor', 'responsable', 'validador_traductor', 'date_m_and_e', 'date_entrega',  'fecha_asignacion_traductor', 'fecha_entrega_traductor', 'script', 'traductorId', 'status_coordinador', 'configuracion', 'bw', 'lockcut', 'netcut', 'final', 'date_bw', 'date_lockcut', 'date_netcut', 'date_final', 'created_at', 'updated_at'];
+    protected $fillable = ['titulo_original', 'num_episodio', 'proyectoId', 'salaId', 'productor', 'responsable', 'validador_traductor', 'date_m_and_e', 'date_entrega',  'fecha_asignacion_traductor', 'fecha_entrega_traductor', 'script', 'traductorId', 'status_coordinador', 'configuracion', 'bw', 'lockcut', 'netcut', 'final', 'date_bw', 'date_lockcut', 'date_netcut', 'date_final', 'created_at', 'updated_at', 'delete_episodio'];
 
     public static function listEpisodios($id)
     {
     	return \DB::table('episodios')
 			->where('proyectoId', '=', $id)
-			->where('delete', '=', false)
+			->where('delete_episodio', '=', false)
 			->get();
     }
 
